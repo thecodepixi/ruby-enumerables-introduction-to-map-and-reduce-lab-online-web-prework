eb806a101853ac6f9ece79ae44a372fec51e14c3
Emily Harber <emily.a.harber@gmail.com>
2019-08-16 22:40:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,3 +21,25 @@ def map_to_no_change(source_array)
   unchanged_array
 
 end
+
+def map_to_double(source_array)
+  doubled_array = []
+  
+  source_array.length.times do |x|
+    doubled_array.push(source_array[x]*2)
+  end
+  
+  doubled_array
+
+end
+
+def map_to_square(source_array)
+  squared_array = []
+  
+  source_array.length.times do |x|
+    squared_array.push(source_array[x]**2)
+  end
+  
+  squared_array
+
+end
