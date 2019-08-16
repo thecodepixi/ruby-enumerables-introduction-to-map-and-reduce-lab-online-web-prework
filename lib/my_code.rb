de87368a45439640913fd7486be8bfdbde456f3c
Emily Harber <emily.a.harber@gmail.com>
2019-08-16 22:59:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@ def map_to_square(source_array)
 end
 
 #totaled array
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   
   total_array = []
   sum = 0
