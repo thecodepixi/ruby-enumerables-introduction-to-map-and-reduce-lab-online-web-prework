81f73787e393bd52d9fe3579b3c40a29b396be50
Emily Harber <emily.a.harber@gmail.com>
2019-08-16 22:38:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,13 @@ def map_to_negativize(source_array)
   negative_array
 
 end
+def map_to_no_change(source_array)
+  negative_array = []
+  
+  source_array.length.times do |x|
+    negative_array.push(source_array[x] * -1)
+  end
+  
+  negative_array
+
+end
